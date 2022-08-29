f71c1b005a4f852669858e4e4972a68a9414d310
TikYokoyama <tiaki.yokoyama97@gmail.com>
2022-08-29 11:32:45 -0300

    1 Commit - Base do projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home(){
+    return(
+        <div className={`flex h-screen justify-center items-center bg-gradient-to-r from-orange-400 to-yellow-400`}>
+            <span className="text-4xl">oi</span>
+        </div>
+    )
+}
