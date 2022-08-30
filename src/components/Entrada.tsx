c3f62c708f7f2cea96a285c4d6d1df591ebb14f9
TikYokoyama <tiaki.yokoyama97@gmail.com>
2022-08-30 17:29:11 -0300

    4 Commit - corrigindo pequenos erros

--- src/components/Entrada.tsx
+++ src/components/Entrada.tsx
@@ -10,8 +10,7 @@ export default function Entrada(props: EntradaProps) {
         <textarea
             className={` flex flex-col mb-1 w-1/2 border border-yellow-500 rounded-lg bg-orange-50 focus:outline-none px-4 py-2 ${props.somenteLeitura ? '' : 'focus:bg-white'}
             `}
-            maxLength="200"
-            type='text'
+            // maxLength="200"
             placeholder={props.texto}
             value={props.valor}
             onChange={e => props.valorMudou?.(e.target.value)} />
